ec57fdcc3c84d7caa037ca5a3b56078439cc5c06
Ana-teston <anapaulateston@gmail.com>
2022-09-21 08:58:45 +0200

    Add document file for fonts

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,35 @@
-import Document, {Html, Main, Head, NextScript} from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
-
-class MyDocument extends Document  {
+class MyDocument extends Document {
     render() {
         return (
-            <Html>
-                <Head></Head>
+            <Html lang="en">
+                <Head>
+                    <link
+                        rel="preload"
+                        href="/fonts/IBMPlexSans-Bold.ttf"
+                        as="font"
+                        crossOrigin="anonymous"
+                    ></link>
+                    <link
+                        rel="preload"
+                        href="/fonts/IBMPlexSans-Regular.ttf"
+                        as="font"
+                        crossOrigin="anonymous"
+                    ></link>
+                    <link
+                        rel="preload"
+                        href="/fonts/IBMPlexSans-SemiBold.ttf"
+                        as="font"
+                        crossOrigin="anonymous"
+                    ></link>
+                </Head>
                 <body>
-                    <Main></Main>
-                    <NextScript />
+                <Main></Main>
+                <NextScript />
                 </body>
             </Html>
-        )
+        );
     }
 }
 export default MyDocument;
