249242db3de6247073e09abeb3bd549dad293ad1
Ana-teston <anapaulateston@gmail.com>
2022-10-14 16:02:09 +0200

    fetching foursquare API for fetch coffee stores in Berlin

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,17 @@ import Banner from "../components/banner";
 import Image from "next/image";
 import Card from "../components/card.component";
 import coffeeStoresData from "../data/coffe-store.json"
+import {fetchCoffeeStores} from "../lib/coffee-stores";
 
 export async function getStaticProps(context) {
     console.log(" Hi getStaticProps");
+
+    const coffeeStores = await fetchCoffeeStores();
+
     return {
         props: {
-            coffeeStores: coffeeStoresData,
-        },
+            coffeeStores,
+        },//will be passed to the page component as props
     };
 }
 
@@ -39,10 +43,12 @@ export default function Home(props) {
                       { props.coffeeStores.map((coffeeStore) => {
                           return(
                               <Card
-                              key={coffeeStore.id}
+                              key={coffeeStore.fsq_id}
                               name={coffeeStore.name}
-                              imgUrl={coffeeStore.imgUrl}
-                              href={`/coffee-store/${coffeeStore.id}`}
+                              imgUrl={coffeeStore.imgUrl ||
+                                  "https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80"
+                              }
+                              href={`/coffee-store/${coffeeStore.fsq_id}`}
                               />
                           );
                       })}
